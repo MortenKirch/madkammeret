8b047f937a481a13cdcf44f3f5108025daa23c79
MortenKirch <morten-kris@live.dk>
2024-12-03 12:10:16 +0100

    deploy?

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export', // Ensures Next.js generates a static export
+    basePath: '/madkammeret', // Replace <REPOSITORY_NAME> with the name of your GitHub repo
+    assetPrefix: '/madkammeret/', // Required for loading static assets
+  };
+  
+  export default nextConfig;
+  
